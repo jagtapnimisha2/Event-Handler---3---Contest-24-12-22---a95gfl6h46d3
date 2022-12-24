b0eb4e5a75360dd77e493933ecd8b315fb8952b7
jagtapnimisha2 <120504401+jagtapnimisha2@users.noreply.github.com>
2022-12-24 15:52:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,17 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
-  const handleInput = (event) =>{
-   // use console.log
+  const handleClick = (event) =>{
+    // use console.log
+    console.log(`Button id is:-${event.target.id}`);
+
   }
 
-  // do not change id of input elements
+  // do not remove the two buttons or change their id
   return (
     <div id="main">
-      <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} />
-
-      <br/>
-      <br/>
-
-      <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} />
-      <br/>
+      <button id="button-a" onClick={handleClick}>Button A</button>
+      <button id="button-b" onClick={handleClick}>Button B</button>
     </div>
   )
 }
